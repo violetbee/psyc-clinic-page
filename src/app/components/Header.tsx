401dd6a0a5132b9181f32d2d82ab49b7e10f1128
Violetbee <caglarkarahuseyin28@gmail.com>
2023-07-11 16:51:24 +0300

    accordion mobile menu ok

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,21 +1,26 @@
+'use client';
+
+import { Icons } from '@/lib/Icons';
 import { Great_Vibes, Sen } from 'next/font/google';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const greatVibes = Great_Vibes({ subsets: ['latin'], weight: ['400'] });
 
 const sen = Sen({ subsets: ['latin'], weight: ['400'] });
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <header
       className={`bg-white border-b-[1px] sticky z-10 top-0  ${sen.className}`}
     >
+      {/* Desktop Menu */}
       <div className='flex justify-between lg:container lg:max-w-7xl mx-auto px-4 py-5 lg:pt-10'>
         <ul className='hidden lg:flex gap-5 mt-3'>
-          {/* create hover over underline transition */}
           <li className='group relative cursor-pointer'>
             Anasayfa
-            {/* hover over underline transition */}
             <div
               className='absolute w-full h-[2px] bg-gray-950 -bottom-1 left-0 transform scale-x-0 group-hover:scale-x-75 transition duration-300
             ease-in-out'
@@ -24,7 +29,6 @@ export const Header = () => {
 
           <li className='group relative cursor-pointer'>
             Hakkımda
-            {/* hover over underline transition */}
             <div
               className='absolute w-full h-[2px] bg-gray-950 -bottom-1 left-0 transform scale-x-0 group-hover:scale-x-75 transition duration-300
             ease-in-out'
@@ -32,39 +36,21 @@ export const Header = () => {
           </li>
           <li className='group relative cursor-pointer'>
             Yazılar
-            {/* hover over underline transition */}
             <div
               className='absolute w-full h-[2px] bg-gray-950 -bottom-1 left-0 transform scale-x-0 group-hover:scale-x-75 transition duration-300
             ease-in-out'
             ></div>
           </li>
         </ul>
-        {/* mobile hamburger menu hover over animated */}
 
         <div className='flex lg:hidden'>
           <div className='relative flex'>
             <button
               type='button'
               className='inline-flex justify-center items-center w-full rounded-lg leading-6 text-sm font-medium text-gray-950 hover:bg-gray-100 focus:outline-none'
-              id='options-menu'
-              aria-haspopup='true'
-              aria-expanded='true'
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
             >
-              <svg
-                className='h-7 w-7'
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-                aria-hidden='true'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M4 6h16M4 12h16M4 18h16'
-                />
-              </svg>
+              <Icons.toggle />
             </button>
           </div>
         </div>
@@ -76,10 +62,8 @@ export const Header = () => {
           Mehmet Fatih Kınık
         </Link>
         <ul className='hidden lg:flex gap-5 mt-3'>
-          {/* create hover over underline transition */}
           <li className='group relative cursor-pointer'>
             Duyurular
-            {/* hover over underline transition */}
             <div
               className='absolute w-full h-[2px] bg-gray-950 -bottom-1 left-0 transform scale-x-0 group-hover:scale-x-75 transition duration-300
             ease-in-out'
@@ -87,7 +71,6 @@ export const Header = () => {
           </li>
           <li className='group relative cursor-pointer'>
             Videolar
-            {/* hover over underline transition */}
             <div
               className='absolute w-full h-[2px] bg-gray-950 -bottom-1 left-0 transform scale-x-0 group-hover:scale-x-75 transition duration-300
             ease-in-out'
@@ -95,7 +78,58 @@ export const Header = () => {
           </li>
           <li className='group relative cursor-pointer'>
             İletişim
-            {/* hover over underline transition */}
+            <div
+              className='absolute w-full h-[2px] bg-gray-950 -bottom-1 left-0 transform scale-x-0 group-hover:scale-x-75 transition duration-300
+            ease-in-out'
+            ></div>
+          </li>
+        </ul>
+      </div>
+
+      {/* Mobile Menu */}
+      <div
+        className={`lg:hidden ${
+          isMenuOpen ? 'h-[calc(100vh-70px)]' : 'h-0'
+        } bg-white border-b-[1px] overflow-hidden w-full absolute z-[999] duration-300`}
+      >
+        <ul className='flex h-full flex-col items-center justify-evenly '>
+          <li className='group relative cursor-pointer text-3xl'>
+            Anasayfa
+            <div
+              className='absolute w-full h-[2px] bg-gray-950 -bottom-1 left-0 transform scale-x-0 group-hover:scale-x-75 transition duration-300
+            ease-in-out'
+            ></div>
+          </li>
+          <li className='group relative cursor-pointer text-3xl'>
+            Hakkımda
+            <div
+              className='absolute w-full h-[2px] bg-gray-950 -bottom-1 left-0 transform scale-x-0 group-hover:scale-x-75 transition duration-300
+            ease-in-out'
+            ></div>
+          </li>{' '}
+          <li className='group relative cursor-pointer text-3xl'>
+            Yazılar
+            <div
+              className='absolute w-full h-[2px] bg-gray-950 -bottom-1 left-0 transform scale-x-0 group-hover:scale-x-75 transition duration-300
+            ease-in-out'
+            ></div>
+          </li>
+          <li className='group relative cursor-pointer text-3xl'>
+            Duyurular
+            <div
+              className='absolute w-full h-[2px] bg-gray-950 -bottom-1 left-0 transform scale-x-0 group-hover:scale-x-75 transition duration-300
+            ease-in-out'
+            ></div>
+          </li>
+          <li className='group relative cursor-pointer text-3xl'>
+            Videolar
+            <div
+              className='absolute w-full h-[2px] bg-gray-950 -bottom-1 left-0 transform scale-x-0 group-hover:scale-x-75 transition duration-300
+            ease-in-out'
+            ></div>
+          </li>
+          <li className='group relative cursor-pointer text-3xl'>
+            İletişim
             <div
               className='absolute w-full h-[2px] bg-gray-950 -bottom-1 left-0 transform scale-x-0 group-hover:scale-x-75 transition duration-300
             ease-in-out'
