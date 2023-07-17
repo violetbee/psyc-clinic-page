ff342d43625d8540f1b3ac90cc49d1c57fe92fcd
unknown <caglarkarahuseyin28@gmail.com>
2023-07-17 23:19:07 +0300

    login production

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -11,11 +11,13 @@ export default function SignInPage() {
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
+    console.log('login process started');
     signIn('credentials', {
       email,
       password,
       callbackUrl: '/admin',
     });
+    console.log('login process ended');
   };
 
   return (
