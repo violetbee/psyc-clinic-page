16c2b9445410b3b949aa4a3becce547adc6b229e
unknown <caglarkarahuseyin28@gmail.com>
2023-08-06 14:22:21 +0300

    tag added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Provider from '../components/Provider';
+import Script from 'next/script';
+
+const GTM_ID = 'GTM-5Q5J2JMP';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,8 +21,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <Script id='google-tag-manager' strategy='afterInteractive'>
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       <body suppressHydrationWarning={true} className={inter.className}>
         <Provider>{children}</Provider>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
       </body>
     </html>
   );
